ceb4e2cb99fa00f88ac57592adbd0fe3e279336d
EmpiresDesMachines <empiresdesmachines@gmail.com>
2024-01-28 19:27:00 +0300

    changing the colorscheme and statusline

--- nvim/lua/config/utils.lua
+++ nvim/lua/config/utils.lua
@@ -8,37 +8,37 @@ if vim.g.colors_name == 'cobalt2' then
 end
 
 -- Pretty Statusline
-vim.cmd [[
-set laststatus=2
-set statusline=
-set statusline+=%1*\ %{StatuslineMode()}                  " Mode
-set statusline+=%1*\ %<%F\                                " File+path
-set statusline+=%1*\ %=\%y\                               " FileType
-set statusline+=%1*\ %{''.(&fenc!=''?&fenc:&enc).''}      " Encoding
-set statusline+=%1*\ %{(&bomb?\",BOM\":\"\")}\            " Encoding2
-set statusline+=%1*\%{&ff}\                               " FileFormat(dos/unix..)
-set statusline+=%1*\ LN:%l/%L\                            " LineNumber
-set statusline+=%1*\ Col:%03c\                            " ColNumber
-
-function! StatuslineMode()
-  let l:mode=mode()
-  if l:mode==#"n"
-    return "NORMAL"
-  elseif l:mode==?"v"
-    return "VISUAL"
-  elseif l:mode==#"i"
-    return "INSERT"
-  elseif l:mode==#"R"
-    return "REPLACE"
-  elseif l:mode==?"s"
-    return "SELECT"
-  elseif l:mode==#"t"
-    return "TERMINAL"
-  elseif l:mode==#"c"
-    return "COMMAND"
-  elseif l:mode==#"!"
-    return "SHELL"
-  endif
-endfunction
-]]
-
+-- vim.cmd [[
+-- set laststatus=2
+-- set statusline=
+-- set statusline+=%1*\ %{StatuslineMode()}                  " Mode
+-- set statusline+=%1*\ %<%F\                                " File+path
+-- set statusline+=%1*\ %=\%y\                               " FileType
+-- set statusline+=%1*\ %{''.(&fenc!=''?&fenc:&enc).''}      " Encoding
+-- set statusline+=%1*\ %{(&bomb?\",BOM\":\"\")}\            " Encoding2
+-- set statusline+=%1*\%{&ff}\                               " FileFormat(dos/unix..)
+-- set statusline+=%1*\ LN:%l/%L\                            " LineNumber
+-- set statusline+=%1*\ Col:%03c\                            " ColNumber
+-- 
+-- function! StatuslineMode()
+--   let l:mode=mode()
+--   if l:mode==#"n"
+--     return "NORMAL"
+--   elseif l:mode==?"v"
+--     return "VISUAL"
+--   elseif l:mode==#"i"
+--     return "INSERT"
+--   elseif l:mode==#"R"
+--     return "REPLACE"
+--   elseif l:mode==?"s"
+--     return "SELECT"
+--   elseif l:mode==#"t"
+--     return "TERMINAL"
+--   elseif l:mode==#"c"
+--     return "COMMAND"
+--   elseif l:mode==#"!"
+--     return "SHELL"
+--   endif
+-- endfunction
+-- ]]
+-- 
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,17 @@
+-- return {
+--   "GertjanReynaert/cobalt2-vim-theme",
+--   priority = 999,
+--   config = function()
+--     vim.cmd([[colorscheme cobalt2]])
+--   end,
+}
+
 return {
-  "GertjanReynaert/cobalt2-vim-theme",
+  "lalitmee/cobalt2.nvim",
   priority = 999,
-  config = function()
-    vim.cmd([[colorscheme cobalt2]])
+  -- event = { "ColorSchemePre" }, -- if you want to lazy load
+  dependencies = { "tjdevries/colorbuddy.nvim" },
+  init = function()
+    require("colorbuddy").colorscheme("cobalt2")
   end,
 }
---
--- return {
---   "lalitmee/cobalt2.nvim",
---     priority = 999,
---     -- event = { "ColorSchemePre" }, -- if you want to lazy load
---     dependencies = { "tjdevries/colorbuddy.nvim" },
---     init = function()
---         require("colorbuddy").colorscheme("cobalt2")
---     end,
--- }
